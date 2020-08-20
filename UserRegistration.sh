115be98ef2c9d23f7411243c7a7c42bd71ac2348
rahulraju69 <rahulraju9550@gmail.com>
2020-08-20 14:06:50 +0530

    pre-defined mobile format

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
 shopt -s extglob
-echo "enter email:"
-read email
-pat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,5}([.][a-zA-Z]{2,3})*$"
-if [[ $email =~ $pat ]]
+echo "enter mobile number:"
+read number
+pat="^[0-9]{2}[[:space:]][0-9]{10}$"
+if [[ $number =~ $pat ]]
 then
-        echo valid
+        echo "number is valid"
 else
-        echo invalid
+        echo "number is invalid"
 fi
 
